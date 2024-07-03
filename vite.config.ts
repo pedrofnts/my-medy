8018e1f8d79afadb5ee844758a3ad27147fc9986
Pedro Fontes <pedro7fontes@gmail.com>
2024-07-02 21:05:13 -0300

    build: updating tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tsconfigPaths({ root: __dirname }), react()],
   build: {
     rollupOptions: {
       output: {
